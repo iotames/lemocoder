1a34ac6f93198ae0db81ef209e2a090efc6ebabf
wuhanqing <554553400@qq.com>
2022-07-07 18:06:55 +0800

    add gin

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -5,19 +5,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 type WebServer interface {
 	ListenAndServe() error
 }
 
-func debug(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "---Success--Debug----r:%+v--\n\n---os.Getenv(\"DB_HOST\"):(%v)----os.Getenv(\"DB_TYPE\"):(%v)---", *r, os.Getenv("DB_HOST"), os.Getenv("DB_TYPE"))
+// func debug(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "---Success--Debug----r:%+v--\n\n---os.Getenv(\"DB_HOST\"):(%v)----os.Getenv(\"DB_TYPE\"):(%v)---", *r, os.Getenv("DB_HOST"), os.Getenv("DB_TYPE"))
+// }
+
+func gdebug(c *gin.Context) {
+	c.String(200, fmt.Sprintf("---Success--Debug-----\n\n---os.Getenv(\"DB_HOST\"):(%v)----os.Getenv(\"DB_TYPE\"):(%v)---", os.Getenv("DB_HOST"), os.Getenv("DB_TYPE")))
 }
 
 func New() WebServer {
 	log.Println("====Begin---webserver--Run")
-	server := http.Server{Addr: "127.0.0.1:8085"}
-	http.HandleFunc("/debug", debug)
+	h := gin.Default()
+	h.GET("/gdebug", gdebug)
+	server := http.Server{Addr: ":8085", Handler: h}
+	// http.HandleFunc("/debug", debug)
 	return &server
 }

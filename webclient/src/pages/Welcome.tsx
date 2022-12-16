8c2d5da035d7cff8fb99dc35c03b6d642971df1a
wuhanqing <554553400@qq.com>
2022-12-16 17:53:25 +0800

    ADD  Timeline to Welcome.tsx

--- webclient/src/pages/Welcome.tsx
+++ webclient/src/pages/Welcome.tsx
@@ -1,46 +1,76 @@
 import { PageContainer } from '@ant-design/pro-components';
 
-import { Alert, Card, Typography, Row, Col } from 'antd';
+import { Alert, Card, Typography, Row, Timeline, Col } from 'antd';
 import React from 'react';
-import styles from './Welcome.less';
+import { SmileOutlined } from '@ant-design/icons';
+// import styles from './Welcome.less';
 
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+// const CodePreview: React.FC = ({ children }) => (
+//   <pre className={styles.pre}>
+//     <code>
+//       <Typography.Text copyable>{children}</Typography.Text>
+//     </code>
+//   </pre>
+// );
 
 const Welcome: React.FC = () => {
 
   return (
     <PageContainer>
       <Card>
-        <Alert message={"开发神器: 代码自动生成工具"} type="success" showIcon banner style={{margin: -12, marginBottom: 24, }} />
+        {/* <Alert message={"开发神器: 代码自动生成工具"} type="success" showIcon banner style={{margin: -12, marginBottom: 24, }} /> */}
+        <Row>
+          <Col span={16}>
+            <Timeline>
+              <Timeline.Item color="green">后端开发环境</Timeline.Item>
+              <Timeline.Item color="green">前端开发环境</Timeline.Item>
+              <Timeline.Item color="red">
+                <p>Solve initial network problems 1</p>
+                <p>Solve initial network problems 2</p>
+                <p>Solve initial network problems 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item>
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item color="gray">
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item color="gray">
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item color="#00CCFF" dot={<SmileOutlined />}>
+                <p>Custom color testing</p>
+              </Timeline.Item>
+            </Timeline>
 
-      <Row>
-      <Typography.Text strong>
+          </Col>
+          <Col span={8}>
+            <Row style={{ marginBlockEnd: 16 }}>
+              <Typography.Text strong>
                 <a href="https://procomponents.ant.design/components" target="__blank" >Ant Design Pro 组件</a>
               </Typography.Text>
-      </Row>
-
-
-
-        <Card>
-        <CodePreview>go mod tidy</CodePreview>
-        <CodePreview>go run . init</CodePreview>
-        <CodePreview>go run </CodePreview>
-        </Card>
-
-
-        <Row>
-        <Typography.Text strong>
+            </Row>
+            <Row style={{ marginBlockEnd: 16 }}>
+              <Typography.Text strong>
                   <a href="https://ant.design/components/overview-cn/" rel="noopener noreferrer" target="__blank" >Ant Design 组件</a>
-                </Typography.Text>
+              </Typography.Text>
+            </Row>
+          </Col>
         </Row>
 
- 
+        {/* 
+          <Card>
+          <CodePreview>go mod tidy</CodePreview>
+          <CodePreview>go run . init</CodePreview>
+          </Card> 
+        */}
+
       </Card>
     </PageContainer>
   );

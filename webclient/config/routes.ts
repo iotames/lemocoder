2ce2df0c04e2fa5c2575682f6bb47e38718c9f3c
wuhanqing <554553400@qq.com>
2022-12-19 16:48:01 +0800

    table col Editable

--- webclient/config/routes.ts
+++ webclient/config/routes.ts
@@ -1,20 +1,16 @@
 export default [
     // { path: '/', component: '@/pages/index' },
     // { path: '/welcome',name: 'welcome',icon: 'smile',component: './Welcome',},
-    // {path: '/tabledemo',name: '数据表格示例',component: './TableDemo',},
     { layout: false,  path: '/public/login',  name: '',  component: './public/Login',   },
     { layout: false,  path: '/public/init',  name: '',  component: './public/AppInit',   },
     { layout: true,  path: '/welcome',  name: 'Welcome',  component: './Welcome',   },
-    { layout: true,  path: '/tabledemo',  name: '数据表格示例',  component: './TableDemo',   },
+    // { layout: true,  path: '/tabledemo',  name: '数据表格示例',  component: './TableDemo',   },
     { layout: true,  path: '/codemaker',  name: '代码生成器',  component: './CodeMaker',   },
-    { layout: true,  path: '/test',  name: '测试页面',  component: './Test',   },
-    { layout: true,  path: '/excelspider',  name: 'Excel爬虫',  component: './ExcelSpider',   },
+    // { layout: true,  path: '/test',  name: '测试页面',  component: './Test',   },
+    // { layout: true,  path: '/excelspider',  name: 'Excel爬虫',  component: './ExcelSpider',   },
     // Code generated Begin; DO NOT EDIT.
     // Code generated End; DO NOT EDIT.
     { layout: true,  path: '/tableschema',  name: '',  component: './TableSchema',   },
     { layout: true,  path: '/',  name: '',   redirect: '/welcome',  },
     { layout: true,  name: '',  component: './404',   },
-    
-    // { path: '/', redirect: '/welcome' },
-    // { component: './404' },
 ];
